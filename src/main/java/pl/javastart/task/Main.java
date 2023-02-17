39029dc9d78e1b8af9ee64d0f11dbb4b9686654c
Jan Termion <jan.termion@gmail.com>
2023-02-17 19:42:15 +0100

    wydzielilem 2 osobne metody do tworzenia tabeli oraz do jej wypelnienia i wydruku.

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,33 +1,12 @@
 package pl.javastart.task;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        int numberOfRows;
-        int numberOfColumns;
-        String divisionLine = "\n";
+        Logic logic = new Logic();
+        int[][] array = logic.createTable();
+        logic.fillAndPrintTable(array);
 
-        System.out.println("Podaj liczbę wierszy.");
-        numberOfRows = scanner.nextInt();
-        System.out.println("Podaj liczbę kolumn");
-        numberOfColumns = scanner.nextInt();
-
-        int[][] array = new int[numberOfRows][numberOfColumns];
-
-        for (int i = 0; i < numberOfColumns; i++) {
-            divisionLine = (divisionLine + "------");
-        }
-
-        for (int i = 0; i < numberOfRows; i++) {
-            for (int j = 0; j < numberOfColumns; j++) {
-                array[i][j] = i * j;
-                System.out.printf("%5d|", array[i][j]);
-            }
-            System.out.println(divisionLine);
-        }
     }
 }
